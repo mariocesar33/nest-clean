f5cd2a51231c0d3426c13f1814e6bbf3fbaba66e
mariocesar33 <mcgato33@hotmail.com>
2024-01-05 10:25:08 -0100

    Usando ConfigModule no Nest.js

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
 import { AppModule } from './app.module'
+import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     // logger: false,
   })
-  await app.listen(3333)
+
+  // app.get => eu consigo buscar os serviços que estão dentro da minha aplicação
+  const configService: ConfigService<Env, true> = app.get(ConfigService)
+
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
