00cfbea4707535e107166ff192e65d0576b4521b
mariocesar33 <mcgato33@hotmail.com>
2024-01-08 14:12:26 -0100

    Gerando token JWT

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Post } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 // const createAccountBodySchema = z.object({
-//  name: z.string(),
-//  email: z.string().email(),
-//  password: z.string().min(6),
+//   name: z.string(),
+//   email: z.string().email(),
+//   password: z.string(),
 // })
 
 // type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
